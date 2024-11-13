d471f6d4f0933f45ea7bdf551161b15c30cb7c93
ezgaro <ezgaro2005@gmail.com>
2024-11-13 23:15:26 +0200

    Adding the responsive landing page

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import hero from "../assets/hero.png";
+
+export default function Hero() {
+  return (
+    <div>
+      <img
+        src={hero}
+        className="w-full max-h-[600px] object-cover"
+        alt="AI generated photo of a big burger"
+      />
+    </div>
+  );
+}
