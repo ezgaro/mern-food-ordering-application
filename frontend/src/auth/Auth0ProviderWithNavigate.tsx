2cfd55734d54b5ff5ab7f8bbc989281e87ebf6e9
ezgaro <ezgaro2005@gmail.com>
2024-11-15 10:18:43 +0200

    Validation Access token

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,4 @@
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
@@ -9,21 +9,23 @@ export default function Auth0ProviderWithNavigate({ children }: Props) {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URI;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+
   const navigate = useNavigate();
 
-  if (!domain || !clientId || !redirectUri) {
+  if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error(
       "Missing environment variables for Auth0. Please check .env file."
     );
   }
-  function onRedirectCallback(appState?: AppState, user?: User) {
+  function onRedirectCallback() {
     navigate("/auth-callback");
   }
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      authorizationParams={{ redirect_uri: redirectUri }}
+      authorizationParams={{ redirect_uri: redirectUri, audience }}
       onRedirectCallback={onRedirectCallback}
     >
       {children}
