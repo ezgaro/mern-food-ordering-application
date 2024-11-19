8152d704cb5e4a179be72f6571cfb86d86af9614
ezgaro <ezgaro2005@gmail.com>
2024-11-19 18:05:57 +0200

    Adding the validation for the restaurant requests

--- backend/src/routes/MyRestaurantRoute.ts
+++ backend/src/routes/MyRestaurantRoute.ts
@@ -2,6 +2,7 @@ import express from "express";
 import multer from "multer";
 import MyRestaurantController from "../controllers/MyRestaurantController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyRestaurantRequest } from "../middleware/validation";
 
 const router = express.Router();
 const storage = multer.memoryStorage();
@@ -14,9 +15,10 @@ const upload = multer({
 
 router.post(
   "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
   jwtCheck,
   jwtParse,
-  upload.single("imageFile"),
   MyRestaurantController.createMyResturant
 );
 
