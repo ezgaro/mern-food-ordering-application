c40695fc84ff35a57c15109ea33bdf41276da0f5
ezgaro <ezgaro2005@gmail.com>
2024-11-24 09:46:07 +0200

    search api added

--- backend/src/controllers/MyRestaurantController.ts
+++ backend/src/controllers/MyRestaurantController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import Restaurant from "../../models/restaurant";
+import Restaurant from "../models/restaurant";
 import { v2 as cloudinary } from "cloudinary";
 import mongoose from "mongoose";
 
